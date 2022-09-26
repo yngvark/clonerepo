9fcbd11f2373cb26079959ffca9ee11094e50566
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2022-09-26 18:48:45 +0200

    Remove lint warnings

--- cmd/testhelper_test.go
+++ cmd/testhelper_test.go
@@ -2,10 +2,12 @@ package cmd_test
 
 type TestGitter struct{}
 
+//goland:noinspection GoUnusedParameter
 func (t TestGitter) Clone(gitUri string, targetCloneDir string) error {
 	return nil
 }
 
+//goland:noinspection GoUnusedParameter
 func (t TestGitter) Pull(gitCloneDir string) error {
 	return nil
 }
