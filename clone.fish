4e39e8f02cbf330dc010addfb2414b32b2ebdf28
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2023-01-23 21:38:19 +0100

    fix: Clone alias for Fish

--- clone.fish
+++ clone.fish
@@ -1,12 +1,7 @@
 function clone --wraps='/home/yngvar/.go/bin/clonerepo ' --wraps='cd $(/home/yngvar/.go/bin/clonerepo )' --description 'alias clone=cd $(/home/yngvar/.go/bin/clonerepo )'
-  #echo $argv[1]
+  set -l OUT $(clonerepo $argv)
 
-  switch $argv[1]
-      case "git@*"
-        cd $(clonerepo $argv)
-      case "https*"
-        cd $(clonerepo $argv)
-      case '*'
-        clonerepo $argv
+  if test (count $OUT) -eq 1
+    cd $(clonerepo $argv)
   end
 end
